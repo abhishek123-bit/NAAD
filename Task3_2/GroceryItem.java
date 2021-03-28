71c63e5479f6e286dfc6293223831009241522dc
Abhi <abhishek7340128579@gmail.com>
2021-03-28 21:24:18 +0530

    Task 3_2 is completed

---
+++ Task3_2/GroceryItem.java
@@ -0,0 +1,11 @@
+package Task3_2;
+
+public class GroceryItem {
+    String name;
+    int PricePerKg;
+    public GroceryItem(String name , int PricePerKg) {
+        this.name=name;
+        this.PricePerKg=PricePerKg;
+    }
+
+}
